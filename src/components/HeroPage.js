71a5bd3c4dc51e6d83e51364f826d94140b3e110
jonathan bleibdrey <jonbleibdrey@aol.com>
2022-03-09 09:42:34 -0500

    added fonts and background fro hero page

--- src/components/HeroPage.js
+++ src/components/HeroPage.js
@@ -4,8 +4,8 @@ import '../css/Heropage.css'
 const HeroPage = () => {
   return (
     <div id="hero" className="content">
-      <h1>Im A Real World Data Magician.</h1>
-      <p>"Ive got the looks and the bronze to accomplish anything."</p>
+      <h1>Real World Evidence | Strategy | Innovation</h1>
+      <p>"Passion for data innovation to make an impact on patient's lives"</p>
       <a
         href="https://www.linkedin.com/in/jonathan-bleibdrey-4032171b9/"
         target="_blank"
--- src/components/Icons.js
+++ src/components/Icons.js
@@ -8,7 +8,7 @@ const Icons = () => {
       <div>
         <i className="fa-solid fa-user-group fa-2x"></i>
         <div>
-          <h3>Team Player</h3>
+          <h3>Industy Expertise</h3>
           <p>
             I believe that you cant go through this world as a lone wolf. You
             have to play well with others, especially if you dont agree with
@@ -19,7 +19,7 @@ const Icons = () => {
       <div>
         <i className="fa-solid fa-person-running fa-2x"></i>
         <div>
-          <h3>Multi Tasker</h3>
+          <h3>Publications Planning</h3>
           <p>
             I have worked In high stressed situations in the streets of NYC
             from my background in film and photography.
@@ -29,7 +29,7 @@ const Icons = () => {
       <div>
         <i className="fa-solid fa-spray-can-sparkles fa-2x"></i>
         <div>
-          <h3>Creative Problem Solver</h3>
+          <h3>RWE Strategy</h3>
           <p>
             I love to solve problems, It teaches me peace. Life is about
             tackling the hard stuff, without losing your cool.
