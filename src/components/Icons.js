ff60efbd96cec3defdae025eaf95fe050bb0ee20
jonathan bleibdrey <jonbleibdrey@aol.com>
2022-03-31 11:09:55 -0400

    added icons

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -6,23 +6,20 @@ const Icons = () => {
     <section id="about" className="icons bg-light">
     <div className="flex-items">
       <div>
-        <i className="fa-solid fa-user-group fa-2x"></i>
+        <i className="fa-solid fa-person-running fa-2x"></i> 
         <div>
           <h3>Industy Expertise</h3>
           <p>
-            I believe that you cant go through this world as a lone wolf. You
-            have to play well with others, especially if you dont agree with
-            the other person.
+          I am an experienced pharmaceutical industry professional who has worked on integrated evidence and strategy planning. As well as helped develop IEGPs for specific assets working closely with cross-functional client teams.
           </p>
         </div>
       </div>
       <div>
-        <i className="fa-solid fa-person-running fa-2x"></i>
+        <i className="fa-solid fa-user-group fa-2x"></i> 
         <div>
           <h3>Publications Planning</h3>
           <p>
-            I have worked In high stressed situations in the streets of NYC
-            from my background in film and photography.
+          worked on observational study design, chart reviews, health outcomes, global value dossiers, integrated evidence strategic planning and has deep knowledge of coding within observational healthcare databases. 
           </p>
         </div>
       </div>
@@ -31,8 +28,7 @@ const Icons = () => {
         <div>
           <h3>RWE Strategy</h3>
           <p>
-            I love to solve problems, It teaches me peace. Life is about
-            tackling the hard stuff, without losing your cool.
+          Experience in developing RWE vision, strategy and roadmaps. She also supports building a global center of excellence in setting up scientific standards for RWE research. 
           </p>
         </div>
       </div>
